ed85665ac48bf136d51d37ae6b5475f894ed7e1d
Yasmine Radouani <yasmine.radouani@outlook.fr>
2021-10-15 10:44:55 +0200

    dernière modification

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const helmet = require ('helmet');
 const dotenv = require ('dotenv');
 const morgan = require ('morgan');
+const cors = require('cors')
 // import mysql from 'promise-mysql';
 const sequelize = require ('./sequelize.js');
 const path = require ('path');
@@ -11,15 +12,16 @@ const path = require ('path');
 // const { fileURLToPath }= require ('url');
 
 const app = express();
-
 app
   .use(helmet())
   .use('/images', express.static(path.join(__dirname, 'images')))
   .use(morgan('dev'))
+  .use(cors())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Credentials', true)
     next();
   });
 
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore } from 'vuex';
 import axios from 'axios';
 const http = axios.create({
-  baseURL: 'https://localhost:3000/api/'
+  baseURL: 'https://localhost:3000/api/auth'
 });
 
 // --------
